33a68c9cf77f50555654b6fee2bec329db898e03
Kaitlin Berryman <kaitlinberryman@gmail.com>
2022-05-29 20:42:41 -0400

    Begin adding tests

--- flask/test_user_model.py
+++ flask/test_user_model.py
@@ -14,18 +14,15 @@ from models import db, User, Message, Follows
 # to use a different database for tests (we need to do this
 # before we import our app, since that will have already
 # connected to the database
-
 os.environ['DATABASE_URL'] = "postgresql:///warbler-test"
 
 
 # Now we can import app
-
 from app import app
 
 # Create our tables (we do this here, so we only create the tables
 # once for all tests --- in each test, we'll delete the data
 # and create fresh new clean test data
-
 db.create_all()
 
 
@@ -35,9 +32,24 @@ class UserModelTestCase(TestCase):
     def setUp(self):
         """Create test client, add sample data."""
 
-        User.query.delete()
-        Message.query.delete()
-        Follows.query.delete()
+        db.drop_all()
+        db.create_all()
+
+        testUser1 = User.signup("testUser1", "testUser1@gmail.com", "password", None)
+        testUser1.id = 101
+
+        testUser2 = User.signup("testUser2", "testUser2@gmail.com", "password", None)
+        testUser2.id = 102
+
+
+        db.session.commit()
+
+
+        testUser1 = User.query.get(testUser1.id)
+        testUser2 = User.query.get(testUser2.id)
+
+        self.testUser1 = testUser1
+    
 
         self.client = app.test_client()
 
